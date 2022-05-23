27ecf2e3e780b42e1a9cf45c1910088f24e2fe3b
Mahadeb Sen <mahadebsen237@gmail.com>
2022-05-23 21:12:40 +0530

    banner added

--- src/App.js
+++ src/App.js
@@ -1,23 +1,25 @@
-import logo from './logo.svg';
-import './App.css';
+import { Route, Routes } from "react-router-dom";
+import "./App.css";
+import About from "./Pages/About/About";
+import Blogs from "./Pages/Blogs/Blogs";
+import Dashboard from "./Pages/Dashboard/Dashboard";
+import Home from "./Pages/Home/Home";
+import Login from "./Pages/Login/Login";
+import MyPortfolio from "./Pages/MyPortfolio/MyPortfolio";
+import Navbar from "./Pages/Navbar/Navbar";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App max-w-7xl mx-auto">
+      <Navbar></Navbar>
+      <Routes>
+        <Route path="/" element={<Home />}></Route>
+        <Route path="/blogs" element={<Blogs />}></Route>
+        <Route path="/myportfolio" element={<MyPortfolio />}></Route>
+        <Route path="/dashboard" element={<Dashboard />}></Route>
+        <Route path="/login" element={<Login />}></Route>
+        <Route path="/about" element={<About />}></Route>
+      </Routes>
     </div>
   );
 }
---
+++ src/Pages/MyPortfolio/MyPortfolio.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const MyPortfolio = () => {
+  return (
+    <div>
+      <p>this is My portfolio</p>
+    </div>
+  );
+};
+
+export default MyPortfolio;
